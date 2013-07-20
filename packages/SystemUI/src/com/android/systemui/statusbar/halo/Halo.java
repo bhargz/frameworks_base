decbbb9c29f19dd80fc8c09cd0414b163ada8043
drcmda <drcmda@gmail.com>
2013-07-20 14:27:29 +0100

    HALO NEW ICON SET (XHDPI ONLY!)
    Change-Id: I83f6bba92c14688a98c09a4722e195b6b01ce93b

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -823,7 +823,8 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
                             } else if (deltaIndex > 0 && mGesture != Gesture.DOWN2) {
                                 mGesture = Gesture.DOWN2;
                                 gestureChanged = true;
-                                mEffect.setHaloOverlay(HaloProperties.Overlay.SILENCE, 1f);
+                                mEffect.setHaloOverlay(mTickerLeft ? HaloProperties.Overlay.SILENCE_LEFT
+                                        : HaloProperties.Overlay.SILENCE_RIGHT, 1f);
                                 gestureText = mContext.getResources().getString(R.string.halo_silence);
                             } 
                         }
